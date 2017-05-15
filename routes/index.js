59e93b52eefcf489baca985e34a3eb02fd4cebc4
Žiga Černigoj <zc6857@student.uni-lj.si>
2017-05-15 21:53:13 +0200

    sklopi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SSKJ' });
+  res.render('index', { title: 'SSZJ' });
 });
 
 module.exports = router;
--- routes/test.js
+++ routes/test.js
@@ -1,46 +1,71 @@
-var express = require('express');
-var router = express.Router();
-var dbBesedajson = require('./beseda.json');
-
-
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host: 'sql203.000a.biz',
-//     user: 'a000b_12082540',
-//     password: '000aPIKAb1z',
-//     database: 'a000b_12082540_sszj'
-//     // host: 'localhost',
-//     // user: 'root',
-//     // password: '',
-//     // database: 'sszj'
+// var express = require('express');
+// var router = express.Router();
+// var dbBesedajson = require('./beseda.json');
+// var fs = require('fs');
+//
+// var b64 = require('./b64');
+// var GIFEncoder = require('./GIFEncoder');
+//
+// var GifEncoder = require('gif-encoder');
+//
+// var LZWEncoder = require('./LZWEncoder');
+// var NeuQuant = require('./NeuQuant');
+// var Canvas = require('canvas');
+//
+// var getPixels = require("get-pixels");
+//
+// router.get('/', function (req, res, next) {
+//
+//     // var canvas = document.getElementById('bitmap');
+//     // var canvas = new Canvas(6000, 256);
+//     // var context = canvas.getContext('2d');
+//
+//     //http://sszj.fri.uni-lj.si/datoteke/sprites_low/a.jpg
+//
+//
+//
+//     fs.readFile(__dirname + '/raw.png', function (err, data) {
+//         if (err) throw err;
+//         var img = new Canvas.Image; // Create a new Image
+//         img.src = data;
+//
+//         console.log(data);
+//
+//         // Initialiaze a new Canvas with the same dimensions
+//         // as the image, and get a 2D drawing context for it.
+//         var canvas = new Canvas(img.width, img.height);
+//         var context = canvas.getContext('2d');
+//         context.drawImage(img, 0, 0, img.width, img.height);
+//
+//
+//         getPixels(__dirname + '/raw.png', function (err, pixels) {
+//             if (err) {
+//                 console.log("Bad image path");
+//                 return;
+//             }
+//
+//
+//             var pxls = context.getImageData(0, 0, img.width, img.height).data;
+//
+//             var file = require('fs').createWriteStream('img.gif');
+//
+//             var gif = new GifEncoder(img.width, img.height);
+//             gif.pipe(file);
+//             gif.writeHeader();
+//             gif.addFrame(pxls);
+//             // gif.addFrame(pixels); // Write subsequent rgba arrays for more frames
+//             gif.finish();
+//
+//             console.log("got pixels", pixels);
+//         });
+//
+//
+//     });
+//
+//     res.send("finished");
+//
+//
 // });
 //
-// connection.connect();
-
-
-function getWord(word) {
-    return dbBesedajson.filter(
-        function (dbBesedajson) {
-            return dbBesedajson.beseda_prava === word
-        }
-    );
-}
-
-
-router.get('/', function (req, res, next) {
-
-    var searchBeseda = req.query.q;
-    // console.log(req.query.beseda);
-
-    if (searchBeseda !== undefined) {
-        var foundWord = getWord(searchBeseda);
-        res.send(foundWord);
-    }
-    else {
-        res.send(dbBesedajson);
-    }
-
-});
-
-module.exports = router;
-// connection.end();
+// module.exports = router;
+// // connection.end();
